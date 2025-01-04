9a00fa480199a0b42ca69c9b0cc4bdce3356565a
Rohith-Yarramsetty <93549206+Rohith-Yarramsetty@users.noreply.github.com>
2025-01-04 13:55:49 +0530

    Print the count of presence of elements of one array in another array (#15)

---
+++ java/Contributors/Rohith/Arrays/CountOfRepeatingArrayElementsInAnotherArray.java
@@ -0,0 +1,30 @@
+// arr1 = {1, 1, 2, 3, 6, 3, 4, 5, 6, 6, 7, 8}
+// arr2 = {1. 3. 6, 8}
+// output: 
+// 1 exists 2 times
+// 3 exists 2 times
+// 6 exists 3 times
+// 8 exists 1 times
+
+package Arrays;
+
+import java.util.*;
+
+public class CountOfRepeatingArrayElementsInAnotherArray {
+    public static void main(String[] args) {
+        int randomArray[] = {1, 2, 4, 6, 9, 11, 2, 16, 5, 6, 9, 18, 23, 18};
+        int numbersToBeChecked[] = {2, 3, 4, 9, 18};
+
+        for(int i = 0; i < numbersToBeChecked.length; i++) {
+            int countOfNumber = 0;
+
+            for(int j = 0; j < randomArray.length; j++) {
+                if(numbersToBeChecked[i] == randomArray[j]) 
+                    countOfNumber ++;
+            }
+
+
+            System.out.println(numbersToBeChecked[i] + " exists " + countOfNumber + " no.of times in the array");
+        }
+    }
+}
